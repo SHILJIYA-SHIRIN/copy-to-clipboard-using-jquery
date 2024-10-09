d366c3deea5a4037a7ff84d95d08a3b70b7f539e
SHILJIYA SHIRIN <sshiljiya@gmail.com>
2024-10-09 19:20:35 +0530

    removed the first code for the js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-// $(document).ready(function (){
-//     $("#copyButton").click(function(){
-//         var input= $("#inputFieldText").val();
-//         navigator.clipboard.writeText(input).then(()=>{
 
-//             alert("the text is copied to clipboard");
-//         })
-//     })
-// })
 $(document).ready(function () {
     $("#copyButton").click(function () { // Remove unnecessary 'copyTheField(text)'
       const input = $("#inputFieldText").val(); // Get the value from input field
